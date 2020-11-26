4bff13859d799f90602528bc2cd051e11fa98e14
LIA <lvxing29@naver.com>
2020-11-26 13:59:24 +0900

    db options

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ const app = express()
 
 //DB connect
 const dbAddress = "mongodb+srv://admin:qwer@cluster0.huxry.mongodb.net/nodeshop1?retryWrites=true&w=majority"
+const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true }
 
 mongoose
-    .connect(dbAddress)
+    .connect(dbAddress, dbOptions)
     .then(() => console.log("DB 연결 성공 "))
     .catch(err => console.log("+++++++++++++", err.message))
 
