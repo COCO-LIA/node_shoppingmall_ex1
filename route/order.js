a6103177bbaa8c972e52649b9a12bfc093d360e0
LIA <lvxing29@naver.com>
2020-12-01 16:37:18 +0900

    Add register order

--- route/order.js
+++ route/order.js
@@ -4,99 +4,29 @@ const router = express.Router()
 const orderModel = require('../model/order')
 
 //전체 데이터 GET
-router.get("/od", (req, res) => {
-    // res.json({
-    //     message: "order get"
-    // })
-
-    orderModel
-        .find()
-        .then(docs => {
-            res.json({
-                msg: "total get",
-                count: docs.length,
-                order: docs.map(doc => {
-                    return {
-                        id: doc._id,
-                        name: doc.name,
-                        price: doc.price,
-                        category: doc.category,
-                        request: {
-                            type: 'GET',
-                            url: "http://localhosst:5010/oorder/" + doc._id
-                        }
-                    }
-                })
-            })
-        })
-        .catch(err => {
-            res.json({
-                msg: err.message
-            })
-        })
+router.get("/", (req, res) => {
+    res.json({
+        message: "order get"
+    })
 })
 
-//상세 데이터 GET
-router.get("/:orderId", (req, res) => {
-    orderModel
-        .findById(req.params.orderId)
-        .then(item => {
-            res.json({
-                msg: "get order data" + item._id,
-                order: {
-                    id: item._id,
-                    name: item.name,
-                    price: item.price,
-                    category: item.category,
-                    reqest: {
-                        type: 'GET',
-                        url: "http://localhost:5010/oorder/od"
-                    }
-                }
-            })
-        })
-        .catch(err => {
-            res.json({
-                msg: err.message
-            })
-        })
-})
-
+//등록해주는 API
 router.post("/", (req, res) => {
-    //
-    // const orderInfo = {
-    //     name: req.body.productname,
-    //     price: req.body.productprice,
-    //     category: req.body.category
-    // }
-    //
-    //
     // res.json({
-    //     msg: "order 등록 API",
-    //     order: orderInfo
+    //     msg: "post"
     // })
 
     const orderInfo = new orderModel({
-        name: req.body.ordername,
-        price: req.body.orderprice,
-        category: req.body.category
+        product: req.body.productId,
+        quantity: req.body.qty
     })
 
     orderInfo
         .save()
         .then(item => {
             res.json({
-                msg: "saved",
-                orderInfo: {
-                    id : item._id,
-                    name : item.name,
-                    price : item.price,
-                    category: item.category,
-                    request: {
-                        type: 'GET',
-                        url: "http://localhost:5010/oorder/" + item._id
-                    }
-                }
+                msg: "장바구니 담기",
+                orderInfo:item
             })
         })
         .catch(err => {
@@ -104,80 +34,21 @@ router.post("/", (req, res) => {
                 msg: err.message
             })
         })
+
 })
 
-router.patch("/:orderId", (req, res) => {
-    // res.json({
-    //     msg: "order 수정 API"
-    // })
-    const updateOps = {}
-
-    for (const ops of req.body) {
-        updateOps[ops.propName] = ops.value
-    }
-
-    orderModel
-        .findByIdAndUpdate(req.params.orderId, { $set: updateOps })
-        .then(() => {
-            res.json({
-                msg: "updated order" + req.params.orderId,
-                request: {
-                    type: 'GET',
-                    url: "http://localhost:5010/oorder/" + req.params.orderId
-                }
-            })
-        })
-        .catch(err => {
-            res.json({
-                msg: err.message
-            })
-        })
-
-
+router.patch("/", (req, res) => {
+    res.json({
+        msg: "order 수정 API"
+    })
 })
 
 //전체 삭제
 router.delete("/", (req, res) =>{
-    // res.json({
-    //     msg: "order 삭제 API"
-    // })
+    res.json({
+        msg: "order 삭제 API"
+    })
 
-    orderModel
-        .deleteMany()
-        .then(() => {
-            res.json({
-                msg: "deleted All",
-                request: {
-                    type: 'GET',
-                    url: "http://localhost:5010/oorder/od"
-                }
-            })
-        })
-        .catch(err => {
-            res.json({
-                msg: err.message
-            })
-        })
-})
-
-//부분 삭제
-router.delete("/:orderId", (req, res) => {
-    orderModel
-        .findByIdAndDelete(req.params.orderId)
-        .then(() => {
-            res.json({
-                msg: "deleted order",
-                request:{
-                    type: 'GET',
-                    url: "http://localhost:5010/oorder/od"
-                }
-            })
-        })
-        .catch(err => {
-            res.json({
-                msg: err.message
-            })
-        })
 })
 
 
--- route/product.js
+++ route/product.js
@@ -1,30 +1,179 @@
 const express = require('express')
 const router = express.Router()
 
+const productModel = require('../model/product')
+
 router.get("/pd", (req, res) => {
-    res.json({
-        message: "product msg"
-    })
+    // res.json({
+    //     message: "product msg"
+    // })
+    productModel
+        .find()
+        .then(docs => {
+            res.json({
+                msg: "total get",
+                count: docs.length,
+                order: docs.map(doc => {
+                    return {
+                        id: doc._id,
+                        name: doc.name,
+                        price: doc.price,
+                        category: doc.category,
+                        request: {
+                            type: 'GET',
+                            url: "http://localhosst:5010/pproduct/" + doc._id
+                        }
+                    }
+                })
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
+})
+
+router.get("/:productId", (req, res) => {
+    productModel
+        .findById(req.params.productId)
+        .then(item => {
+            res.json({
+                msg: "get product data" + item._id,
+                order: {
+                    id: item._id,
+                    name: item.name,
+                    price: item.price,
+                    category: item.category,
+                    reqest: {
+                        type: 'GET',
+                        url: "http://localhost:5010/pproduct/pd"
+                    }
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
 })
 
 router.post("/", (req, res) =>{
-    res.json({
-        msg: "product 등록 API"
+    //
+    // const orderInfo = {
+    //     name: req.body.productname,
+    //     price: req.body.productprice,
+    //     category: req.body.category
+    // }
+    //
+    //
+    // res.json({
+    //     msg: "order 등록 API",
+    //     order: orderInfo
+    // })
+
+    const productInfo = new productModel({
+        name: req.body.productname,
+        price: req.body.productprice,
+        category: req.body.category
     })
+
+    productInfo
+        .save()
+        .then(item => {
+            res.json({
+                msg: "saved",
+                productInfo: {
+                    id : item._id,
+                    name : item.name,
+                    price : item.price,
+                    category: item.category,
+                    request: {
+                        type: 'GET',
+                        url: "http://localhost:5010/pproduct/" + item._id
+                    }
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
+
 })
 
 router.patch("/", (req, res) => {
-    res.json({
-        msg: "product 수정 api"
-    })
+    // res.json({
+    //     msg: "product 수정 api"
+    // })
+    const updateOps = {}
+
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+
+    productModel
+        .findByIdAndUpdate(req.params.productId, { $set: updateOps })
+        .then(() => {
+            res.json({
+                msg: "updated product" + req.params.productId,
+                request: {
+                    type: 'GET',
+                    url: "http://localhost:5010/pproduct/" + req.params.productId
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
+
 })
 
 router.delete("/", (req, res) => {
-    res.json({
-        msg: "pro 삭제 api"
-    })
+    // res.json({
+    //     msg: "pro 삭제 api"
+    // })
+
+    productModelModel
+        .deleteMany()
+        .then(() => {
+            res.json({
+                msg: "deleted All",
+                request: {
+                    type: 'GET',
+                    url: "http://localhost:5010/pproduct/pd"
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
+
 })
 
-
+router.delete("/:productId", (req, res) => {
+    productModel
+        .findByIdAndDelete(req.params.productId)
+        .then(() => {
+            res.json({
+                msg: "deleted product",
+                request:{
+                    type: 'GET',
+                    url: "http://localhost:5010/pproduct/pd"
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
+})
 
 module.exports = router
